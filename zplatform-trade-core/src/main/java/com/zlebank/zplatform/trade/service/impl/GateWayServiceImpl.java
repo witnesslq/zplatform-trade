8ca774fe74caa6da01da16c63bc4449e30bbed7b
guojia <guojia@zlebank.com>
2016-06-06 09:32:12 +0800

    修改bug

--- zplatform-trade-core/src/main/java/com/zlebank/zplatform/trade/service/impl/GateWayServiceImpl.java
+++ zplatform-trade-core/src/main/java/com/zlebank/zplatform/trade/service/impl/GateWayServiceImpl.java
@@ -1309,6 +1309,7 @@ public class GateWayServiceImpl extends
 			txnsLog.setAccordcommitime(withdrawBean.getTxnTime());
 			txnsLog.setTradestatflag("00000000");// 交易初始状态
 			txnsLog.setAccmemberid(withdrawBean.getMemberId());
+			txnsLog.setTxnfee(txnsLogService.getTxnFee(txnsLog));
 			txnsLogService.save(txnsLog);
 
 			// 提现账务处理
