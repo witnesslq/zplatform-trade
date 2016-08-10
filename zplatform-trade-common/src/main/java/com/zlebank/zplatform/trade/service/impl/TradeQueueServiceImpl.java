4fff1354c91f6bd9760692c9f8eb977b716d333f
liushoumei <liushoumei@liushoumei-PC>
2016-08-10 11:18:44 +0800

    redisTemplate添加注解

--- zplatform-trade-common/src/main/java/com/zlebank/zplatform/trade/service/impl/TradeQueueServiceImpl.java
+++ zplatform-trade-common/src/main/java/com/zlebank/zplatform/trade/service/impl/TradeQueueServiceImpl.java
@@ -40,7 +40,7 @@ public class TradeQueueServiceImpl implements TradeQueueService{
 	private final static Log log = LogFactory.getLog(TradeQueueServiceImpl.class);
 	
 	
-	
+	@Autowired
 	private RedisTemplate<String, String> redisTemplate; 
 	
 	public void addTradeQueue(TradeQueueBean tradeQueueBean){
